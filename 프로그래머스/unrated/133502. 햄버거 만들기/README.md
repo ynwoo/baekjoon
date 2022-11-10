c1d74355eb60b8cfcfea73fe60354dafadc8691b
김윤우 <ynwookim98@gmail.com>
2022-11-10 23:01:46 +0900

    [unrated] Title: 햄버거 만들기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/133502. 햄버거 만들기/README.md
+++ 프로그래머스/unrated/133502. 햄버거 만들기/README.md
@@ -1,10 +1,10 @@
 # [unrated] 햄버거 만들기 - 133502 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/133502) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/133502#) 
 
 ### 성능 요약
 
-메모리: 10.1 MB, 시간: 0.00 ms
+메모리: 10.2 MB, 시간: 0.00 ms
 
 ### 구분
 
--- 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.py
+++ 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.py
@@ -1,16 +1,14 @@
-from collections import deque
+
 
 
 def solution(ingredient):
     answer = 0
-    stack = deque()
+    stack = []
     for item in ingredient:
         stack.append(item)
-        if len(stack) >= 4 and stack[-4] == 1 and stack[-3] == 2 and stack[-2] == 3 and stack[-1] == 1:
-            stack.pop()
-            stack.pop()
-            stack.pop()
-            stack.pop()
+        if len(stack) >= 4 and stack[-4:] == [1,2,3,1]:
+            for _ in range(4):
+                stack.pop()
             answer += 1
                 
     return answer
