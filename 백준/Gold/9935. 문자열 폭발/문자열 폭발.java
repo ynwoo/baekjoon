64eb8445403b54a463538b91070cc2446ee5914c
김윤우 <ynwookim98@gmail.com>
2023-02-11 11:04:02 +0900

    [Gold IV] Title: 문자열 폭발, Time: 680 ms, Memory: 62864 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.java
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.java
@@ -10,25 +10,23 @@ public class Main {
 
 		String string = br.readLine();
 		String explosionString = br.readLine();
-		int stringLength = string.length();
-		int explosionStringLength = explosionString.length();
-		
+
 		Stack<Character> stack = new Stack<>();
 		Stack<Character> temp = new Stack<>();
 
-		for (int i = stringLength - 1; i >= 0; i--) { // 역순으로 스택에 넣는다.
+		for (int i = string.length() - 1; i >= 0; i--) { // 역순으로 스택에 넣는다.
 			stack.push(string.charAt(i));
 
-			// 폭발문자열의 길이보다 스택의 사이즈가 크거나 같고, 이번에 들어온 문자가 폭발문자열의 맨 앞 문자라면! 
+			// 폭발문자열의 길이보다 스택의 사이즈가 크거나 같고, 이번에 들어온 문자가 폭발문자열의 맨 앞 문자라면!
 			// -> 폭발문자열이 있을 가능성이 있다!
-			if (stack.peek() == explosionString.charAt(0) && explosionStringLength <= stack.size()) {
+			if (stack.peek() == explosionString.charAt(0) && explosionString.length() <= stack.size()) {
 				// 폭발문자열의 길이 만큼 스택에서 pop해서 폭발문자열과 같은지 비교, 같으면 그대로, 다르면 다시 stack에 push
-				for (int j = 0; j < explosionStringLength; j++) {
+				for (int j = 0; j < explosionString.length(); j++) {
 					char c = stack.pop();
 					temp.push(c);
-					if(c != explosionString.charAt(j)) {
+					if (c != explosionString.charAt(j)) { // 폭발문자열과 같지 않음
 						// 다시 스택에 push 해주고 break
-						while(!temp.empty()) {
+						while (!temp.empty()) {
 							stack.push(temp.pop());
 						}
 						break;
@@ -38,12 +36,13 @@ public class Main {
 			}
 			// System.out.println(stack);
 		}
-		
-		if(stack.empty()) {
+
+		if (stack.empty()) {
 			System.out.println("FRULA");
 		} else {
+			// StringBuilder 안쓰면 시간초과!
 			StringBuilder sb = new StringBuilder();
-			while(!stack.empty()) {
+			while (!stack.empty()) {
 				sb.append(stack.pop());
 			}
 			System.out.println(sb.toString());
