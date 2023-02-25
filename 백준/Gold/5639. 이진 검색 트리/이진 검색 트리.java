0c8795bfa230412cec64cfb02f8e4bd43fe32f12
김윤우 <ynwookim98@gmail.com>
2023-02-26 01:01:23 +0900

    [Gold V] Title: 이진 검색 트리, Time: 952 ms, Memory: 19580 KB -BaekjoonHub

--- 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.java
+++ 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.java
@@ -1,5 +1,6 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Stack;
 
 class Node {
 	int key;
@@ -24,19 +25,48 @@ public class Main {
 		// 입력이 없을 때 까지
 		while ((input = br.readLine()) != null && !input.isEmpty()) {
 			n = Integer.parseInt(input);
-			// System.out.println(n);
-			root = insert(root, n);
+			root = insert(root, n); // 삽입
 		}
 
-		// 후위 순회 결과 출력
-		traversal(root);
+		// 순회 결과 출력
+		// traversal(root);
+		traversalUsingStack(root);
 	}
 
+	private static void traversalUsingStack(Node root) {
+		Stack<Node> stack = new Stack<Node>();
+		Stack<Node> print = new Stack<Node>();
+		stack.push(root);
+		while (!stack.isEmpty()) {
+			Node top = stack.pop();
+			print.push(top);
+
+			if (top.left != null) {
+				stack.push(top.left);
+			}
+
+			if (top.right != null) {
+				stack.push(top.right);
+			}
+
+		}
+		while (!print.isEmpty()) {
+			Node top = print.pop();
+			System.out.println(top.key);
+		}
+	}
+
+	// 재귀로 구현한 노드 삽입
+	// node를 루트로 하는 서브트리에 n을 삽입하고 서브트리의 루트노드를 리턴
 	private static Node insert(Node node, int n) {
+		// 1. 노드가 null인 경우 -> 서브트리가 비어있는 경우
+		// n을 추가하면 노드가 n 한개만 있는 서브트리가 생기기 때문에 n노드를 만들고 리턴
 		if (node == null) {
 			return new Node(n);
 		}
 
+		// 2. 노드의 왼쪽 자식에 n이 들어가야 하는 경우(현재 노드의 키보다 작으면 왼쪽 서브트리에 추가)
+		// 왼쪽 서브트리에 n을 삽입하고 그 왼쪽 서브트리의 루트노드를 node의 왼쪽 자식으로 만들고 node 그대로 리턴
 		if (n < node.key) { // 현재 노드의 키보다 작으면 왼쪽 서브트리에 추가
 			node.left = insert(node.left, n);
 		} else if (node.key < n) { // 현재 노드의 키보다 크면 오른쪽 서브트리에 추가
@@ -44,12 +74,14 @@ public class Main {
 		}
 		return node;
 	}
-	
+
 	private static void traversal(Node node) {
-		if(node == null) {
+		if (node == null) {
 			return;
 		}
+		// System.out.println(node.key); // 전위 순회
 		traversal(node.left);
+		// System.out.println(node.key); // 중위 순회
 		traversal(node.right);
 		System.out.println(node.key); // 후위 순회
 	}
