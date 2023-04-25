8c9d9c7d6bcdd3657c69bca1a44be0e3c6ac2ec6
김윤우 <ynwookim98@gmail.com>
2023-04-25 23:02:46 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 73.20 ms, Memory: 81.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+
+class Solution {
+    public int[] solution(String[] gems) {
+        int[] answer = new int[2];
+        int minLength = Integer.MAX_VALUE;
+        
+        Set<String> set = new HashSet<>(Arrays.asList(gems));
+        Map<String, Integer> jMap = new HashMap<>();
+
+        int n = gems.length; // 보석 전체 개수
+        int numOfJewelryTypes = set.size(); // 보석 종류 개수
+        int left = 0;
+        int right = 0;
+        
+        while(true) {
+            //System.out.println(selected);
+            if(jMap.size() == numOfJewelryTypes) {
+                jMap.put(gems[left], jMap.get(gems[left])-1);
+                if (jMap.get(gems[left]) == 0) {
+                    jMap.remove(gems[left]);
+                }
+                left++;
+            } else if(right < n) {
+                jMap.put(gems[right], jMap.getOrDefault(gems[right], 0) + 1);
+                right++;
+            } else {
+                break;
+            }
+                
+            if (jMap.size() == set.size()) {
+                if (right-left < minLength) {
+                    minLength = right-left;
+                    answer[0] = left+1;
+                    answer[1] = right;
+                }
+            }
+            
+        }
+        
+        return answer;
+    }
+}
