d870dc5ee087723f8d3c80243447340057eb64a9
Yoonwoo <ynwookim98@gmail.com>
2024-01-21 23:39:27 +0900

    [Bronze IV] Title: 주사위 세개, Time: 104 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.py
@@ -0,0 +1,16 @@
+from sys import stdin
+
+input = stdin.readline
+
+a, b, c = map(int, input().split())
+
+if a == b == c:
+  print(10000 +  a * 1000)
+elif a == b:
+  print(1000 + a * 100)
+elif b == c:
+  print(1000 + b * 100)
+elif a == c:
+  print(1000 + a * 100)
+else:
+  print(max(max(a,b),c) * 100)
